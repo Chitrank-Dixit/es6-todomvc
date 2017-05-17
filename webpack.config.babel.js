964f3b02aabed34c6d692f1e4adafc5457990647
Chitrank-Dixit <chitrankdixit@gmail.com>
2017-05-18 03:43:49 +0530

    changed a bit

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,22 @@
 /* eslint no-console:"off" */
-const {resolve} = require('path')
-const webpack = require('webpack')
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpackValidator = require('webpack-validator')
-const {getIfUtils, removeEmpty} = require('webpack-config-utils')
-const OfflinePlugin = require('offline-plugin')
+const {resolve} = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpackValidator = require('webpack-validator');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
+const OfflinePlugin = require('offline-plugin');
+
+// there is a tool that validate the webpack properly the tool is webpack validator
+
+// validate-webpack:dev to validate webpack at the development environment just run $ npm run validate-webpack:dev
+
+// tree shaking will allow to reduce bundle (build or dist) sizes for large builds by analyzing exports and imports
+// The less code you can send to the browser, the better. The concept of tree shaking basically says that if you’re not using some piece of code,
+// then exclude it from the final bundle, even when that piece of code is exported from a module. Because ES6 modules are statically analyzable,
+// Webpack can determine which of your dependencies are used and which are not. In this lesson, see how to take advantage of this awesome feature in Webpack 2
 
 module.exports = env => {
   const {ifProd, ifNotProd} = getIfUtils(env)
